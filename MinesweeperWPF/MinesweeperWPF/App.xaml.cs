c40e217f902f7f5d2c60150e2379a9f067a078d1
st_alexisa <sviridov01.alesha@mail.ru>
2022-05-10 17:40:07 +0300

    [add] start menu

---
+++ MinesweeperWPF/MinesweeperWPF/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MinesweeperWPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ MinesweeperWPF/MinesweeperWPF/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MinesweeperWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        
+        private static void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Кнопка нажата");
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            ExitButton.Click += (object sender, RoutedEventArgs e) => { this.Close();};
+        }
+    }
+}
